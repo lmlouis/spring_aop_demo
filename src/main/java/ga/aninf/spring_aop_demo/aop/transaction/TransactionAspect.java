10fb540eaa07dedee69ed8c41d39133e36c8760f
louis-martin.wora <louis-martin.wora@aninf.ga>
2025-02-18 17:10:38 +0100

    Aspect Transaction

---
+++ src/main/java/ga/aninf/spring_aop_demo/aop/transaction/TransactionAspect.java
@@ -0,0 +1,12 @@
+package ga.aninf.spring_aop_demo.aop.transaction;
+
+import org.aspectj.lang.annotation.Aspect;
+
+/**
+ * @author Louis Martin WORA SOUAMY
+ * @description Aspect pour historiser les transactions
+ */
+@Aspect("transaction")
+public class TransactionAspect {
+
+}

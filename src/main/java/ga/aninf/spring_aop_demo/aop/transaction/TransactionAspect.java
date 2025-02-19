f78aaa78d67b3593109840dee477f20385a71add
louis-martin.wora <louis-martin.wora@aninf.ga>
2025-02-19 07:28:53 +0100

    configuration de l'aspect

--- src/main/java/ga/aninf/spring_aop_demo/aop/transaction/TransactionAspect.java
+++ src/main/java/ga/aninf/spring_aop_demo/aop/transaction/TransactionAspect.java
@@ -1,12 +1,108 @@
 package ga.aninf.spring_aop_demo.aop.transaction;
 
+import ga.aninf.spring_aop_demo.domain.Compte;
+import ga.aninf.spring_aop_demo.domain.Transaction;
+import ga.aninf.spring_aop_demo.repository.CompteRepository;
+import ga.aninf.spring_aop_demo.service.TransactionService;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import java.math.BigDecimal;
+import java.util.UUID;
 
 /**
  * @author Louis Martin WORA SOUAMY
  * @description Aspect pour historiser les transactions
  */
-@Aspect("transaction")
+@Aspect
 public class TransactionAspect {
+
+    private final TransactionService transactionService;
+
+    private final CompteRepository compteRepository;
+
+    public TransactionAspect(TransactionService transactionService, CompteRepository compteRepository) {
+        this.transactionService = transactionService;
+        this.compteRepository = compteRepository;
+    }
+
+    /**
+     * Créer une transaction avec les infos de la méthode et les paramètres passés en paramètre
+     * @param joinPoint
+     * @return
+     */
+    public void creerTransaction(JoinPoint joinPoint, Boolean isSuccess) {
+        if (Boolean.FALSE.equals(isSuccess)) return;
+        // Récupération des arguments passés à la méthode interceptée
+        Object[] args = joinPoint.getArgs();
+        UUID compteReference = (UUID) args[0];  // Le premier argument est l'UUID du compte
+        BigDecimal montant = args.length > 1 ? (BigDecimal) args[1] : BigDecimal.ZERO;
+
+        //  Déduction du type de transaction
+        String methodName = joinPoint.getSignature().getName();
+        String typeTransaction = switch (methodName) {
+            case "crediter" -> "CREDIT";
+            case "debiter" -> "DEBIT";
+            case "consulterSolde" -> "CONSULTATION";
+            default -> "INCONNU";
+        };
+        Compte compte = compteRepository.findById(compteReference).orElse(null);
+        String author = compte != null? compte.getTitulaire() : "INCONNU";
+        String description =  Boolean.TRUE.equals(isSuccess) ?
+                String.format("%s  a effectuer avec succès une transaction de %s de %s",author, typeTransaction, montant)
+                : String.format("%s  a echouer une transaction de %s de %s",author, typeTransaction, montant);
+
+        // Création de la transaction
+        Transaction transaction = transactionService.creerTransaction(
+                typeTransaction,
+                montant,
+                description,
+                compteReference
+        );
+
+        // Log pour vérification
+        System.out.println(joinPoint.getSignature().getDeclaringTypeName());
+        System.out.println("Transaction créée : " + transaction);
+    }
+
+    /**
+     * Pointcut pour intercepter les méthodes de création de transaction
+     */
+    @Pointcut("execution(* com.example.service.CompteService.crediter(..)) || " +
+                    "execution(* com.example.service.CompteService.debiter(..)) || " +
+                    "execution(* com.example.service.CompteService.consulterSolde(..))"
+    )
+    public void transactionPointcut() {
+        // Pas de traitement car elle impléments des conseils d'aspects
+    }
+
+    /**
+     * Conseil d'aspect pour les méthodes succès de transaction
+     * @param joinPoint
+     * @param result
+     */
+    @AfterReturning(value = "transactionPointcut()",
+            returning = "result")
+    public void apresTransactionReussie(JoinPoint joinPoint, Object result) {
+        System.out.println("Transaction réussie : " + result);
+        // Appeler la méthode creerTransaction avec succès
+        creerTransaction(joinPoint, Boolean.TRUE);
+    }
+
+    /**
+     * Conseil d'aspect pour les échecs de transaction
+     * @param joinPoint
+     * @param ex
+     */
+    @AfterThrowing(value = "transactionPointcut()",
+            throwing = "ex")
+    public void apresTransactionEchouee(JoinPoint joinPoint, Exception ex) {
+        System.out.println("Transaction échouée : " + ex.getMessage());
+        // Appeler la méthode creerTransaction avec échec
+        creerTransaction(joinPoint, Boolean.FALSE);
+    }
+
 
 }
---
+++ src/main/java/ga/aninf/spring_aop_demo/config/AspectConfig.java
@@ -0,0 +1,9 @@
+package ga.aninf.spring_aop_demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class AspectConfig {
+}
